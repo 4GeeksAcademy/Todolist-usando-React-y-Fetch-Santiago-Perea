5634230154dc96ff0b9fd67a393a1ccafe004a17
santiago-pp <santiagopereaap@gmail.com>
2025-11-10 17:53:24 +0000

    Proyecto terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,43 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [valorInput, setValorInput] = useState("");
+	const [tareas, setTareas] = useState([])
+	const counter = [...tareas].length;
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	function newInput(event) {
+		if (event.code === 'Enter') {
+			setTareas([...tareas, valorInput])
+			setValorInput("");
+		}
+	};
+
+	function deleteInput(deleteTarea) {
+		const newTareas = tareas.filter((tarea) => tarea !== deleteTarea)
+		setTareas(newTareas)
+	}
+
+	return (
+		<div className="container">
+			<h1 className="text-center">To Do List</h1>
+			<div className="toDoBox"> 
+				<ul>
+					<li><input type="text" value={valorInput} onKeyDown={newInput} onChange={(event) => setValorInput(event.target.value)} placeholder="What needs to be done?"></input></li>
+					{tareas.map(tarea => {
+						return <li key={tarea}>{tarea}
+							<button type="button" onClick={() => deleteInput(tarea)} class="btn-close" aria-label="Close"></button>
+						</li>
+
+					})}
+					<p>{counter} items left</p>
+				</ul>
+				
+			</div>
 		</div>
 	);
 };
